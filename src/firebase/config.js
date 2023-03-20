9ab69769fd0d008de2402d7caae141fe01bce367
Tanya <vermatanya932@gmail.com>
2023-03-21 03:44:52 +0530

    Signup and Dashboard Completed

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD_BxyQ9B643FJUI4vVurmtF4Lc-KrGKgs',
+    authDomain: 'oauth3-85c03.firebaseapp.com',
+    projectId: 'oauth3-85c03',
+    storageBucket: 'oauth3-85c03.appspot.com',
+    messagingSenderId: '133725755512',
+    appId: '1:133725755512:web:09075124f4ddb49c7b358d',
+};
+
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
